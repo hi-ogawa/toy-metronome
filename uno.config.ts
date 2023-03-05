a8c590363b226e32c07f2685f035e140b1458855
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-05 18:18:38 +0900

    choer: setup unocss-preset-antd

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,10 @@
+import { antdPreset } from "@hiogawa/unocss-preset-antd";
+import {
+  dummyPreset,
+  dummyPresetIconsRules,
+  filterColorPallete,
+  transformerTypescriptDsl,
+} from "@hiogawa/unocss-typescript-dsl";
 import {
   defineConfig,
   presetIcons,
@@ -5,41 +12,24 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
-import { antdPreset } from "./src/styles/antd-preset";
 
 // based on https://github.com/hi-ogawa/youtube-dl-web-v2/blob/ca7c08ca6b144c235bdc4c7e307a0468052aa6fa/packages/app/uno.config.ts
 
 export default defineConfig({
   presets: [
     antdPreset(),
-    presetUno(),
+    filterColorPallete(presetUno()),
     presetIcons({
       extraProperties: {
         display: "inline-block",
       },
     }),
+    dummyPreset(),
   ],
-  shortcuts: {
-    spinner: `
-      animate-spin
-      rounded-full
-      border-2 border-gray-500 border-t-gray-300 border-l-gray-300
-    `,
-    btn: `
-      cursor-pointer
-      transition
-      disabled:(cursor-not-allowed opacity-50)
-    `,
-    "btn-ghost": `
-      not-disabled:hover:(text-[var(--colorPrimaryHover)])
-      not-disabled:active:(text-[var(--colorPrimaryActive)])
-    `,
-    "btn-primary": `
-      text-white
-      bg-[var(--colorPrimary)]
-      not-disabled:hover:bg-[var(--colorPrimaryHover)]
-      not-disabled:active:bg-[var(--colorPrimaryActive)]
-    `,
-  },
-  transformers: [transformerDirectives(), transformerVariantGroup()],
+  rules: [...dummyPresetIconsRules(["ri"])],
+  transformers: [
+    transformerTypescriptDsl(),
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
 });
