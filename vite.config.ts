62b3cc9d3f30561d3e1e2513f788817d63a67888
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-08 20:29:10 +0900

    refactor: simplify theme switch

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,12 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [unocss(), react(), serviceWorkerPrecachePlugin()],
+  plugins: [
+    unocss(),
+    react(),
+    serviceWorkerPrecachePlugin(),
+    injectThemeScriptPlugin(),
+  ],
 });
 
 function serviceWorkerPrecachePlugin(): Plugin {
@@ -29,6 +34,28 @@ function serviceWorkerPrecachePlugin(): Plugin {
       sw = sw.replace(injectionPoint, JSON.stringify(manifest));
       await fs.promises.mkdir(path.dirname(swDst), { recursive: true });
       await fs.promises.writeFile(swDst, sw);
+    },
+  };
+}
+
+// inject theme initialization script
+function injectThemeScriptPlugin() {
+  const script = fs.readFileSync(
+    require.resolve("@hiogawa/utils-experimental/dist/theme-script.global.js"),
+    "utf-8"
+  );
+  return {
+    name: "local:" + injectThemeScriptPlugin.name,
+    transformIndexHtml(html: string) {
+      return html.replace(
+        /<!--@@INJECT_THEME_SCRIPT@@-->/,
+        `\
+<script>
+  globalThis.__themeStorageKey = "toy-metronome:theme";
+  globalThis.__themeDefault = "dark";
+  ${script}
+</script>`
+      );
     },
   };
 }
