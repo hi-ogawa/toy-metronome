af2ce1f2990564c216d9f77f78f19eb9ddf2d27e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-05 13:17:15 +0900

    refactor: simplify audio context management

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import { registerServiceWorker } from "./service-worker/window";
+import { initAudioContext } from "./utils/audio-context";
 
 function main() {
   if (import.meta.env.PROD) {
     registerServiceWorker(); // hunging promise
   }
+  initAudioContext();
   const el = document.querySelector("#root");
   tinyassert(el);
   const root = createRoot(el);
