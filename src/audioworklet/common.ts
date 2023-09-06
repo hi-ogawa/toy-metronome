67657d089147d9cab3a12b706720f84f4fe78c5b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-06 13:37:22 +0900

    chore: remove unused zod

--- src/audioworklet/common.ts
+++ src/audioworklet/common.ts
@@ -1,14 +1,1 @@
-import { z } from "zod";
-
 export const METRONOME_PROCESSOR_NAME = "metronome";
-
-export const CUSTOM_MESSAGE_SCHEMA = z.discriminatedUnion("type", [
-  z.object({
-    type: z.literal("setState"),
-    data: z.object({ playing: z.boolean() }),
-  }),
-  // unused
-  z.object({ type: z.literal("reset") }),
-]);
-
-export type CustomMessageSchema = z.infer<typeof CUSTOM_MESSAGE_SCHEMA>;
