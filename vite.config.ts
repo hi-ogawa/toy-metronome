18ea0fcbf8c5dabe4225d68007d9f844fa7112fd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-10-07 16:05:22 +0900

    chore(deps): tinyReactVitePlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { themeScriptPlugin } from "@hiogawa/theme-script/dist/vite";
+import { tinyReactVitePlugin } from "@hiogawa/tiny-react/dist/plugins/vite";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      react: "@hiogawa/tiny-react",
-    },
-  },
   build: {
     sourcemap: true,
   },
   plugins: [
     unocss(),
+    tinyReactVitePlugin(),
     themeScriptPlugin({
       storageKey: "toy-metronome:theme",
       defaultTheme: "dark",
