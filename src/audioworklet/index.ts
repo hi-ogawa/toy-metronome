89983035b2135694e0ff43effdcf8a8facd79426
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-22 11:51:00 +0900

    chore: investigate https://github.com/vitejs/vite/discussions/15036

--- src/audioworklet/index.ts
+++ src/audioworklet/index.ts
@@ -1,4 +1,15 @@
 import { METRONOME_PROCESSOR_NAME } from "./common";
 import { MetronomeProcessor } from "./metronome";
 
+import { repro } from "./repro";
+
+function reproOk() {
+  import("./repro")
+}
+
+// this triggers injection of `__vite__injectQuery`
+function reproNotOk() {
+  import("./repro".slice())
+}
+
 registerProcessor(METRONOME_PROCESSOR_NAME, MetronomeProcessor);
